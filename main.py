9543bb0491ff745f45fc9964a4c69d66531c9940
nanato12 <admin@okj.info>
2023-11-25 05:27:49 +0900

    update main.py

--- main.py
+++ main.py
@@ -9,15 +9,25 @@ from spotify.constants.enum.time_range import TimeRange
 load_dotenv(verbose=True)
 
 spotify = Spotify(environ["ACCESS_TOKEN"])
+
+# show your profile
 print(spotify.get_profile())
+
+# show smedjan's profile
 print(spotify.get_user("smedjan"))
+
+# follow paylist (id: 3cEYpjA9oz9GiPac4AsH4n)
 print(spotify.follow_playlist("3cEYpjA9oz9GiPac4AsH4n", True))
+
+# unfollow paylist (id: 3cEYpjA9oz9GiPac4AsH4n)
 print(spotify.unfollow_playlist("3cEYpjA9oz9GiPac4AsH4n"))
 
 # artists
-spotify.get_top_items(
-    ItemType.ARTISTS, time_range=TimeRange.LONG_TERM, limit=1
+print(
+    spotify.get_top_items(
+        ItemType.ARTISTS, time_range=TimeRange.LONG_TERM, limit=1
+    )
 )
 
 # tracks
-spotify.get_top_items(ItemType.TRACKS, limit=1, offset=1)
+print(spotify.get_top_items(ItemType.TRACKS, limit=1, offset=1))
