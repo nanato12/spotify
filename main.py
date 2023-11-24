54064d8da067b7405be2e345bfef0fb6cb413fee
nanato12 <admin@okj.info>
2023-11-25 05:23:04 +0900

    update: main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 from spotify import Spotify
 from spotify.constants.enum.item_type import ItemType
+from spotify.constants.enum.time_range import TimeRange
 
 load_dotenv(verbose=True)
 
@@ -12,4 +13,11 @@ print(spotify.get_profile())
 print(spotify.get_user("smedjan"))
 print(spotify.follow_playlist("3cEYpjA9oz9GiPac4AsH4n", True))
 print(spotify.unfollow_playlist("3cEYpjA9oz9GiPac4AsH4n"))
-print(spotify.get_top_items(ItemType.TRACKS, limit=1, offset=1))
+
+# artists
+spotify.get_top_items(
+    ItemType.ARTISTS, time_range=TimeRange.LONG_TERM, limit=1
+)
+
+# tracks
+spotify.get_top_items(ItemType.TRACKS, limit=1, offset=1)
