68da411aad11099ac8b089faea8eba7a4c79992d
nanato12 <admin@okj.info>
2023-11-25 05:19:12 +0900

    update: TopItem

--- spotify/objects/top_item.py
+++ spotify/objects/top_item.py
@@ -1,14 +1,15 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 
-from spotify.objects.profile import Profile
+from spotify.objects.artist import Artist
+from spotify.objects.track import Track
 
 
 @dataclass
 class TopItem:
-    items: List[Profile]
+    items: List[Union[Artist, Track]]
     total: int
     limit: int
     offset: int
@@ -20,5 +21,18 @@ class TopItem:
     def from_dict(cls, d: Dict[str, Any]) -> TopItem:
         if d.get("items"):
             items: List[Dict[str, Any]] = d.pop("items")
-            d["items"] = [Profile.from_dict(item) for item in items]
+            result_items = []
+            result_items.extend(
+                [
+                    Artist.from_dict(item)
+                    for item in filter(lambda x: x["type"] == "artist", items)
+                ]
+            )
+            result_items.extend(
+                [
+                    Track.from_dict(item)
+                    for item in filter(lambda x: x["type"] == "track", items)
+                ]
+            )
+            d["items"] = result_items
         return cls(**d)
