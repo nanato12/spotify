d6bcaa7fc9eefc86085bcd8e027d63e894371a10
nanato12 <admin@okj.info>
2023-11-25 04:40:11 +0900

    update: Profile

--- spotify/objects/profile.py
+++ spotify/objects/profile.py
@@ -1,5 +1,7 @@
-from dataclasses import dataclass
-from typing import Any, Dict, List
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Any, Dict, List, Optional
 
 
 @dataclass
@@ -12,3 +14,11 @@ class Profile:
     type: str
     uri: str
     followers: Dict[str, Any]
+    popularity: Optional[int] = None
+    genres: List[str] = field(default_factory=list)
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> Profile:
+        if d.get("name"):
+            d["display_name"] = d.pop("name")
+        return cls(**d)
