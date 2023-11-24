b2d546ef4133d62b5d30c05b9a8713ea2d42945e
nanato12 <admin@okj.info>
2023-11-25 05:19:43 +0900

    update: main.py

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from os import environ
 from dotenv import load_dotenv
 
 from spotify import Spotify
+from spotify.constants.enum.item_type import ItemType
 
 load_dotenv(verbose=True)
 
 spotify = Spotify(environ["ACCESS_TOKEN"])
 print(spotify.get_profile())
 print(spotify.get_user("smedjan"))
+print(spotify.follow_playlist("3cEYpjA9oz9GiPac4AsH4n", True))
+print(spotify.unfollow_playlist("3cEYpjA9oz9GiPac4AsH4n"))
+print(spotify.get_top_items(ItemType.TRACKS, limit=1, offset=1))
