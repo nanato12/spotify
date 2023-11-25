bb874e262bcdc087a88d1691b7db448bf533519f
nanato12 <admin@okj.info>
2023-11-25 11:53:04 +0900

    add: ItemType, PluralItemType

--- spotify/constants/enum/item_type.py
+++ spotify/constants/enum/item_type.py
@@ -2,5 +2,12 @@ from enum import Enum
 
 
 class ItemType(Enum):
+    USER = "user"
+    ARTIST = "artist"
+    TRACK = "track"
+    ALBUM = "album"
+
+
+class PluralItemType(Enum):
     ARTISTS = "artists"
     TRACKS = "tracks"
