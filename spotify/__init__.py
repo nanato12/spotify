857a380913614aacb2a19b66052e63d635214749
nanato12 <admin@okj.info>
2023-11-25 10:40:34 +0900

    update: Spotify

--- spotify/__init__.py
+++ spotify/__init__.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from base64 import b64encode
+from dataclasses import dataclass, field
 from typing import Any, Dict, Type, TypeVar
 
 import requests
@@ -7,44 +8,104 @@ from requests import Response
 from spotify.constants.enum.item_type import ItemType
 from spotify.constants.enum.time_range import TimeRange
 from spotify.constants.url import URL
+from spotify.logger import get_file_path_logger
+from spotify.objects.api_token import ApiToken
 from spotify.objects.profile import Profile
 from spotify.objects.top_item import TopItem
 
-A = TypeVar("A", Profile, TopItem)
+A = TypeVar("A", Profile, TopItem, ApiToken)
+
+logger = get_file_path_logger(__name__)
 
 
 @dataclass
 class Spotify:
-    access_token: str
+    client_id: str
+    client_secret: str
+    refresh_token: str = ""
+    access_token: str = field(init=False, default="")
 
     def __post_init__(self) -> None:
-        pass
+        if self.refresh_token:
+            self.generate_access_token()
 
     @property
     def headers(self) -> Dict[str, str]:
-        return {"Authorization": f"Bearer {self.access_token}"}
+        if self.access_token:
+            return {"Authorization": f"Bearer {self.access_token}"}
+        return {"Authorization": f"Basic {self.credential}"}
+
+    @property
+    def credential(self) -> str:
+        return b64encode(
+            f"{self.client_id}:{self.client_secret}".encode()
+        ).decode("utf-8")
+
+    @staticmethod
+    def __validate_response(r: Response) -> None:
+        r.raise_for_status()
+        logger.debug(r.json())
 
     @staticmethod
     def __convert(r: Response, class_: Type[A]) -> A:
-        print(r.json())
         c: A = class_.from_dict(r.json())
         return c
 
-    def __get(
-        self, url: str, class_: Type[A], params: Dict[str, Any] = {}
-    ) -> A:
+    def __get(self, url: str, params: Dict[str, Any] = {}) -> Response:
         r = requests.get(url, headers=self.headers, params=params)
-        r.raise_for_status()
-        return self.__convert(r, class_)
+        self.__validate_response(r)
+        return r
+
+    def __post(self, url: str, data: Dict[str, Any] = {}) -> Response:
+        r = requests.post(url, headers=self.headers, data=data)
+        self.__validate_response(r)
+        return r
 
     def __put(self, url: str, data: Dict[str, Any] = {}) -> Response:
         r = requests.put(url, headers=self.headers, data=data)
-        r.raise_for_status()
+        self.__validate_response(r)
         return r
 
     def __delete(self, url: str) -> Response:
         r = requests.delete(url, headers=self.headers)
-        r.raise_for_status()
+        self.__validate_response(r)
+        return r
+
+    def generate_refresh_token(self, code: str, redirect_uri: str) -> ApiToken:
+        r = self.__convert(
+            self.__post(
+                URL.API_TOKEN,
+                data={
+                    "grant_type": "authorization_code",
+                    "code": code,
+                    "redirect_uri": redirect_uri,
+                },
+            ),
+            ApiToken,
+        )
+
+        if r.refresh_token:
+            self.refresh_token = r.refresh_token
+        self.access_token = r.access_token
+
+        return r
+
+    def generate_access_token(self) -> ApiToken:
+        if not self.refresh_token:
+            raise ValueError("'refresh_token' must not be empty.")
+
+        r = self.__convert(
+            self.__post(
+                URL.API_TOKEN,
+                {
+                    "grant_type": "refresh_token",
+                    "refresh_token": self.refresh_token,
+                },
+            ),
+            ApiToken,
+        )
+        self.access_token = r.access_token
+
         return r
 
     def get_top_items(
@@ -57,21 +118,25 @@ class Spotify:
         if not 0 <= limit <= 50:
             raise ValueError("'limit' must be between 0 and 50.")
 
-        return self.__get(
-            URL.TOP_ITMES.format(type=type.value),
+        return self.__convert(
+            self.__get(
+                URL.TOP_ITMES.format(type=type.value),
+                params={
+                    "time_range": time_range.value,
+                    "limit": limit,
+                    "offset": offset,
+                },
+            ),
             TopItem,
-            params={
-                "time_range": time_range.value,
-                "limit": limit,
-                "offset": offset,
-            },
         )
 
     def get_profile(self) -> Profile:
-        return self.__get(URL.ME, Profile)
+        return self.__convert(self.__get(URL.ME), Profile)
 
     def get_user(self, user_id: str) -> Profile:
-        return self.__get(URL.USERS.format(user_id=user_id), Profile)
+        return self.__convert(
+            self.__get(URL.USERS.format(user_id=user_id)), Profile
+        )
 
     def follow_playlist(
         self, playlist_id: str, public: bool = False
