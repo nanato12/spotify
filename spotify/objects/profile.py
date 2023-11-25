1fa56958d666e7b089a824f4ef7640c807bb3937
nanato12 <admin@okj.info>
2023-11-25 11:55:25 +0900

    update: Profile

--- spotify/objects/profile.py
+++ spotify/objects/profile.py
@@ -1,24 +1,29 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
-from spotify.objects import SpotifyObject
+from spotify.constants.enum.item_type import ItemType
+from spotify.objects import SpotifyObjectItem
 
 
 @dataclass
-class Profile(SpotifyObject):
-    id: str
-    name: str
-    type: str
-    href: str
-    uri: str
+class Profile(SpotifyObjectItem):
     external_urls: Dict[str, str] = field(default_factory=dict)
     images: List[Dict[str, Any]] = field(default_factory=list)
     followers: Dict[str, Any] = field(default_factory=dict)
 
+    # scope: user-read-private
+    country: Optional[str] = None
+    explicit_content: Dict[str, bool] = field(default_factory=dict)
+    product: Optional[str] = None
+
+    # scope: user-read-email
+    email: Optional[str] = None
+
     @classmethod
     def from_dict(cls, d: Dict[str, Any]) -> Profile:
+        d["type"] = ItemType(d["type"])
         if d.get("display_name"):
             d["name"] = d.pop("display_name")
         return cls(**d)
